4babcd3caf7a5773d9077c189681c2f0d9800d5f
shy <2010249@mail.nankai.edu.cn>
2023-06-10 10:44:22 +0800

    167

--- 167.cpp
+++ 167.cpp
@@ -4,6 +4,15 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        
+        int size = numbers.size();
+        int i = 0, j = size, sum = 0;
+        while((sum = numbers[i]+numbers[j-1]) != target){
+            if(sum > target)
+                j--;
+            else
+                i++;
+        }
+        vector<int> arr = {i+1, j};
+        return arr;
     }
 };
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Leetcode_CPP
 
+> 悟已往之不谏，知来者之可追。——《归去来兮辞》
+
 ### First follow 《[LeetCode 101](https://github.com/changgyhub/leetcode_101)》, finish all chapters.
 
 ### Then choose leetcode problems with specific purpose or randomly.
