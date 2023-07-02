59013d8854ef4da8cb38f87763177966a37f046d
shy <2010249@mail.nankai.edu.cn>
2023-07-02 18:21:10 +0800

    81

---
+++ 81.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        while(l<=r){
+            int mid = l + (r - l) / 2;
+            if(nums[mid] == target)
+                return true;
+            else if(nums[mid] == nums[r])
+                --r;
+            else if(nums[mid] == nums[l])
+                ++l;
+            else if(nums[mid] < nums[r]){
+                if(target>nums[mid] && target<=nums[r])
+                    l = mid + 1;
+                else
+                    r = mid - 1;
+            }else{
+                if(target<nums[mid] && target>=nums[l])
+                    r = mid - 1;
+                else
+                    l = mid + 1;
+            }
+        }
+        return false;
+    }
+};
